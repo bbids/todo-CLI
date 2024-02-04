2ecbcc1b8fd6ed75702301e56d4f2aa8c725c6f9
bbids <82536500+bbids@users.noreply.github.com>
2024-02-04 19:24:32 +0100

    remove old package from setup

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup
 setup(
     name='To-Do CLI',
     version='1.0.0',
-    packages=['todo', 'tests'],
+    packages=['todo'],
     entry_points={
         'console_scripts': [
             'todo = todo.main:main' 
         ]
     },
-    install_requires=["getch"],
 )
